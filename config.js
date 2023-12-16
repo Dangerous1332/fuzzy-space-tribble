0c9af81790d434f805968a9c6389646366b4f988
Shani0835 <134261073+Shani0835@users.noreply.github.com>
2023-12-16 23:28:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,13 +15,13 @@ global.autoRecord = true; //auto recording (true to on, false to off)
 
 
 //
-global.Owner = ["923184687747"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.OwnerNumber = ["923184687747"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.Owner = ["923455116994"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.OwnerNumber = ["923455116994"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number
 global.ownertag = ["923184687747"];
-global.OwnerName = "🦅 ⃟𝙸𝚂𝙼𝙰𝙸𝙻 𝙺𝙸𝙽𝙶᭄";
-global.BotName = "A17";
-global.packname = "A17 Bot";  //Do not change.
-global.author = "By: Kai";   //Do not change.
+global.OwnerName = "*✮°⏜⤹𝐒ɦ𝛂ͥƞι 𝐑𝛂ᴊͣᴘͫʊʈ⤸⏜°✮*";
+global.BotName = "THE-SHANI⁹⁹⁴-MD-BOT";
+global.packname = "✮°⏜⤹𝐒ɦ𝛂ͥƞι 𝐑𝛂ᴊͣᴘͫʊʈ⤸⏜°✮";  //Do not change.
+global.author = "@";   //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17";  //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/LRqRpR4EWXADSVtj6XMtZo"; 
 
@@ -38,7 +38,7 @@ global.openAiAPI = "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";
 global.location = "Tata City, India";
 global.reactmoji = "❤️";
 global.themeemoji = "💖";
-global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
+global.vidmenu = { url: 'https://telegra.ph/file/0a81e721b9fddc71f726e.mp4' };
 global.websitex = "https://github.com/Kai0071";
 global.lolhuman = "KaysaS";
 
